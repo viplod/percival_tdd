2d90949ca51f8b3799afe7756d9ad94eab5d1491
Юра Ананьин <viplod@yandex.ru>
2022-04-24 16:28:13 +0500

    ФТ проверяет возможность ввести элемент списка

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -18,21 +20,26 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-    # Ей сразу же предлагается ввести элемент списка
-    # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби –
-    # вязание рыболовных мушек)
-    # Когда она нажимает enter, страница обновляется, и теперь страница
-    # содержит "1: Купить павлиньи перья" в качестве элемента списка
-    # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
-    # Она вводит "Сделать мушку из павлиньих перьев"
-    # (Эдит очень методична)
-    # Страница снова обновляется, и теперь показывает оба элемента ее списка
-    # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
-    # сайт сгенерировал для нее уникальный URL-адрес – об этом
-    # выводится небольшой текст с объяснениями.
-    # Она посещает этот URL-адрес – ее список по-прежнему там.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+
+        self.fail('Закончить тест!')
 
 
 if __name__ == '__main__':
