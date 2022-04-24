f603f6f99a0b1dc7d09830658d326504ea8c4076
Юра Ананьин <viplod@yandex.ru>
2022-04-24 18:08:17 +0500

    Добавлен функциональный тест для списка

--- functional_tests.py
+++ functional_tests.py
@@ -35,9 +35,19 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows),
-            'Новый элемент списка не появился в таблице'
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn(
+            '2: Сделать мушку из павлиньих перьев',
+            [row.text for row in rows]
         )
 
         self.fail('Закончить тест!')
--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    '''Домашняя страница.'''
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
