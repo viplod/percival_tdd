708ae37fe3ebf09818281ed07021d1b9b9e3fb29
Юра Ананьин <viplod@yandex.ru>
2022-04-24 16:59:23 +0500

    Рефакторизовано представления домашней страницы для шаблона

--- lists/tests.py
+++ lists/tests.py
@@ -1,22 +1,10 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
     '''Тест домашней страницы.'''
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
         '''Тест: url '/' преобразуется в представление домашней страницы'''
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
